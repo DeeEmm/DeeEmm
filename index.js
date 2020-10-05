c27b47575667f5cc706682b1b32114ed421a578c
DeeEmm <deeemm@deeemm.com>
2020-10-05 16:38:27 +0000

    Auto Blog Update

--- index.js
+++ index.js
@@ -2,11 +2,24 @@
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+async function getBlogPosts() {
+  var md = '';
+  let feed = await parser.parseURL('https://deeemm.com/feed.xml');
+    console.log(feed.title);
+    
+    feed.items.forEach(item => {
+          console.log(item.title + ':' + item.link)
+          md += `[` + item.title + `](` + item.link + `)  
+          `
+          ;        
+    });        
+    DATA.blog_feed = md; 
+}
+
+
 
 let DATA = {
   name: 'DeeEmm',
@@ -21,16 +34,19 @@ let DATA = {
   }),
 };
 
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
+
+
+async function generateReadMe() {
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
 	if (err) throw err;
 	const output = Mustache.render(data.toString(), DATA);
 	fs.writeFileSync('README.md', output);
   });
 }
-generateReadMe();
+
+async function action() {
+  await getBlogPosts(); 
+  await generateReadMe();  
+}
+
+action();
